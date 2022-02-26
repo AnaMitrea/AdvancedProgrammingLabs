0d377e9ffc7918f5246df6feea5bfe831fb3bba1
Annee <anamitrea27@gmail.com>
2022-02-26 14:55:42 +0200

    Laboratory 2, Compulsory Project

---
+++ Laboratory2/Compulsory/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<Room> createRooms(ArrayList<Room> rooms) {
+        Room room1 = new Room("401",30,Type.Laboratory);
+        Room room2 = new Room("403",30,Type.Laboratory);
+        Room room3 = new Room("405",30,Type.Laboratory);
+        Room room4 = new Room("309",100,Type.Course);
+        rooms.add(room1);
+        rooms.add(room2);
+        rooms.add(room3);
+        rooms.add(room4);
+        return rooms;
+    }
+
+    public static ArrayList<Event> createEvents(ArrayList<Event> events) {
+        Event event1 = new Event("C1",100,8,10);
+        Event event2 = new Event("C2",100,10,12);
+        Event event3 = new Event("L1",30,8,10);
+        Event event4 = new Event("L2",100,8,10);
+        Event event5 = new Event("L3",100,10,12);
+        events.add(event1);
+        events.add(event2);
+        events.add(event3);
+        events.add(event4);
+        events.add(event5);
+        return events;
+    }
+
+    public static void printRooms(ArrayList<Room> rooms) {
+        System.out.print("Rooms: ");
+        for(Room room : rooms) {
+            System.out.print(room.toString() + " ");
+        }
+    }
+
+    public static void printEvents(ArrayList<Event> events) {
+        System.out.println("Events: ");
+        for(Event event : events) {
+            System.out.print(event.toString() + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Room> rooms= new ArrayList<Room>();
+        rooms = createRooms(rooms);
+        printRooms(rooms);
+
+        ArrayList<Event> events = new ArrayList<Event>();
+        events = createEvents(events);
+        printEvents(events);
+    }
+}
---
+++ Laboratory2/Compulsory/src/main/java/Room.java
@@ -0,0 +1,43 @@
+public class Room {
+    private String name;
+    private int capacity;
+    private Type propertyType;
+
+    public Room(String name, int capacity, Type propertyType) {
+        this.name = name;
+        this.capacity = capacity;
+        this.propertyType = propertyType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Type getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(Type propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    @Override
+    public String toString() {
+        return  name + '(' +
+                "cap=" + capacity +
+                ')';
+    }
+
+}
