f48494bd4c44b1d1bb276dee41a2fc284594f30b
Annee <anamitrea27@gmail.com>
2022-03-01 20:00:21 +0200

    Abstractizarea clasei Room

--- Laboratory2/Homework/src/main/java/Room.java
+++ Laboratory2/Homework/src/main/java/Room.java
@@ -1,3 +1,17 @@
-public class Room {
+public abstract class Room {
+    protected String name;
+    protected int capacity;
+    protected String roomType;
 
+    public abstract String getName();
+
+    public abstract void setName(String name);
+
+    public abstract int getCapacity();
+
+    public abstract void setCapacity(int capacity);
+
+    public abstract String getRoomType();
+
+    public abstract void setRoomType(String roomType);
 }
