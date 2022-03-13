88ebdbb39615edfa24c41d4de7101379e9d8bfdb
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-13 14:05:23 +0200

    Modificat main

--- Laboratory3/Compulsory/src/main/java/Main.java
+++ Laboratory3/Compulsory/src/main/java/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Network network = new Network();
 
-        Node v1 = new Computer("v1", "addr1",8);
-        Node v2 = new Router("v2","addr2");
-        Node v3 = new Switch("v3","addr3");
-        Node v4 = new Switch("v4","addr4");
-        Node v5 = new Router("v5","addr5");
-        Node v6 = new Computer("v6","addr6",16);
+        Node v1 = new Computer("v1", "14.8.100.105",8);
+        Node v2 = new Router("v2","201.215.143.255");
+        Node v3 = new Switch("v3","56.18.89.220");
+        Node v4 = new Switch("v4","14.238.246.51");
+        Node v5 = new Router("v5","245.212.14.137");
+        Node v6 = new Computer("v6","177.122.47.180",16);
 
         network.addNode(v1);
         network.addNode(v4);
