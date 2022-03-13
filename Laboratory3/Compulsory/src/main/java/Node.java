bdd2658b2139b69e894bc5f0274da707849e1b68
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-13 14:05:11 +0200

    Adaugat HashMap

--- Laboratory3/Compulsory/src/main/java/Node.java
+++ Laboratory3/Compulsory/src/main/java/Node.java
@@ -1,11 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Node implements Comparable<Node>{
     protected String name;
+    protected Map<Node,Integer> cost = new HashMap<>();
 
     abstract public String getName();
-
     abstract public void setName(String name);
-
     abstract public String toString();
+
+    public void setCost(Node node,int value) {
+        cost.put(node, value);
+    }
 
     @Override
     public int compareTo(Node other) {
