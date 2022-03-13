13bb680d9837308e40e55fe84948baa8fc8a45b7
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-13 16:00:18 +0200

    main

--- Laboratory3/Compulsory/src/main/java/Main.java
+++ Laboratory3/Compulsory/src/main/java/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
         Node v1 = new Computer("v1", "14.8.100.105",8);
         Node v2 = new Router("v2","201.215.143.255");
-        Node v3 = new Switch("v3","56.18.89.220");
-        Node v4 = new Switch("v4","14.238.246.51");
+        Node v3 = new Switch("v3");
+        Node v4 = new Switch("v4");
         Node v5 = new Router("v5","245.212.14.137");
         Node v6 = new Computer("v6","177.122.47.180",16);
 
@@ -16,6 +16,8 @@ public class Main {
         network.addNode(v6);
         network.addNode(v5);
 
-        System.out.println(network.toString());
+        System.out.println(network);
+
+        network.printSortedIdentifiable();
     }
 }
