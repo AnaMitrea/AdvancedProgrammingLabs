ac11a9f9505cb3f1f0c02887f746ce4a28f4e127
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-13 14:04:47 +0200

    Conversie GB in: MB, KB si B

--- Laboratory3/Compulsory/src/main/java/Storage.java
+++ Laboratory3/Compulsory/src/main/java/Storage.java
@@ -1,4 +1,16 @@
 public interface Storage {
-    int getStorageCapacity();
-    void setStorageCapacity(int storageCapacity);
+    double getStorageCapacity();
+    void setStorageCapacity(double storageCapacity);
+
+    default double getStorageCapacityInMegaByte(double storageCapacity) {
+        return storageCapacity * 1024;
+    }
+
+    default double getStorageCapacityInKyloByte(double storageCapacity) {
+        return storageCapacity * 1_048_576;
+    }
+
+    default double getStorageCapacityInByte(double storageCapacity) {
+        return storageCapacity * 1_073_741_824;
+    }
 }
