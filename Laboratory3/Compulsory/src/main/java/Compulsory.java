a57bca5a6fe714ef29a8645bb521ae73765445fe
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-14 11:15:03 +0200

    Compulsory separat

--- Laboratory3/Compulsory/src/main/java/Compulsory.java
+++ Laboratory3/Compulsory/src/main/java/Compulsory.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Compulsory {
     public static void main(String[] args) {
         Network network = new Network();
 
@@ -17,7 +17,5 @@ public class Main {
         network.addNode(v5);
 
         System.out.println(network);
-
-        network.printSortedIdentifiable();
     }
 }
