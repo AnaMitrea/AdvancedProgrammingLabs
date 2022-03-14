ecf615a2b23a82657d7d769ac71e18304652acdc
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-14 15:58:20 +0200

    Compulsory

--- Laboratory4/Lab4/src/main/java/Compulsory.java
+++ Laboratory4/Lab4/src/main/java/Compulsory.java
@@ -1,2 +1,23 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class Compulsory {
+    public static void main(String[] args) {
+        var intersections = IntStream.rangeClosed(0, 8)
+                .mapToObj(i -> new Intersection("v" + i) )
+                .toArray(Intersection[]::new);
+
+        List<Street> streetList = new LinkedList<>();
+        for(int i = 0; i < 15; i++) {
+            streetList.add(new Street("s" + i, new Random().nextInt((3 - 1) + 1) + 1));
+        }
+        System.out.println("Unsorted streetList: " + streetList);
+
+        List<Street> sortedStreetList = new LinkedList<>(streetList);
+        sortedStreetList.sort(((u, v) -> u.getLength().compareTo(v.getLength())));
+        System.out.println("\nSorted streetList: " + sortedStreetList);
+
+        Set<Street> streetHashSet = new HashSet<>(sortedStreetList);
+        System.out.println("\nStreet set: " + streetHashSet);
+    }
 }
---
+++ Laboratory4/Lab4/src/main/java/Street.java
@@ -0,0 +1,35 @@
+public class Street {
+    private String name;
+    private int length;
+
+    public Street(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Street(" +
+                name +
+                ", " + length +
+                "km) ";
+    }
+
+
+}
