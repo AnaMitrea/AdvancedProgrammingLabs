c5e5ca97c173f35c1fae847917601d44530b7d50
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-14 11:15:20 +0200

    Modificare atribute din Node

--- Laboratory3/Compulsory/src/main/java/Node.java
+++ Laboratory3/Compulsory/src/main/java/Node.java
@@ -1,16 +1,41 @@
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public abstract class Node implements Comparable<Node>{
     protected String name;
     protected Map<Node,Integer> cost = new HashMap<>();
+    protected Integer distance = Integer.MAX_VALUE;
+    protected List<Node> shortestPath = new LinkedList<>();
 
     abstract public String getName();
     abstract public void setName(String name);
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
     abstract public String toString();
 
-    public void setCost(Node node,int value) {
-        cost.put(node, value);
+    public void setCost(Node key, int value) {
+        cost.put(key, value);
+    }
+
+    public Map<Node, Integer> getCost() {
+        return cost;
+    }
+
+    public List<Node> getShortestPath() {
+        return shortestPath;
+    }
+
+    public void setShortestPath(List<Node> shortestPath) {
+        this.shortestPath = shortestPath;
     }
 
     @Override
