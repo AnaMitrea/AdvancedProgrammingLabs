2fb1325094219661c1b9ea80a3212705cafd2d31
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-03-25 14:57:31 +0200

    Report command

--- Laboratory5/Project/src/main/java/solution/Homework.java
+++ Laboratory5/Project/src/main/java/solution/Homework.java
@@ -4,10 +4,13 @@ import catalog.*;
 import commands.*;
 import item.Item;
 import item.bibliographic_reference.*;
+import utility.CatalogUtil;
+
+import java.io.IOException;
 
 public class Homework {
-    public static void main(String[] args) {
-        Catalog catalog = new Catalog("Books & Articles");
+    public static void main(String[] args) throws IOException {
+        Catalog catalog = new Catalog("My Catalog");
         Item book = new Book("knuth67","The Art of Computer Programming","d:/books/programming/tacp.ps", "1967", "Donald E. Knuth", "PublishingHouse1");
         Item article = new Article("java17","The Java Language Specification","https://docs.oracle.com/javase/specs/jls/se17/html/index.html","2021", "James Gosling & others", "JournalTitle1");
 
@@ -16,13 +19,15 @@ public class Homework {
         addCommand.add(catalog,book);
         addCommand.add(catalog,article);
 
+        CatalogUtil.saveCatalog(catalog,"target/catalog.json");
+
         ListCommand listCommand = new ListCommand();
         listCommand.describeCommand();
         listCommand.list(catalog);
 
         ViewCommand viewCommand = new ViewCommand();
         viewCommand.describeCommand();
-        //viewCommand.view("catalog.json");
+        viewCommand.view("catalog.json");
 
         ReportCommand reportCommand = new ReportCommand();
         reportCommand.describeCommand();
